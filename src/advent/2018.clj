7d9f563ad63679352105951de2dc924633555c76
Manuel Rascioni <mrascioni@hotels.com>
2018-12-11 17:48:28 +0100

    base macro for exercise

--- src/advent/2018.clj
+++ src/advent/2018.clj
@@ -1,1 +1,5 @@
-(ns advent.2018)
+(ns advent.2018
+  (:require [advent.core :as a]))
+
+(a/defcase day1 "2018/1.input.txt"
+  (println (read *in*)))
--- src/advent/core.clj
+++ src/advent/core.clj
@@ -1,13 +1,11 @@
 (ns advent.core
   (:require [clojure.java.io :as io]))
 
-(defmacro with-input [file body]
-    `(binding [*in* (->> ~file
-                     io/resource
-                     io/input-stream
-                     io/reader
-                     `(new clojure.lang.LineNumberingPushbackReader))])
-     ~body)
-
-(defmacro prova [a]
-  `(println ~a))
+(defmacro defcase [name file body]
+  `(defn ~name []
+     (binding [*in* (->> ~file
+                      io/resource
+                      io/input-stream
+                      io/reader
+                      (new clojure.lang.LineNumberingPushbackReader))]
+       ~body)))
