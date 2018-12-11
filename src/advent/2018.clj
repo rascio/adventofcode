e3eeebbbbe4fc9e7462a4394c62b0e9dcecfa1cd
Manuel Rascioni <mrascioni@hotels.com>
2018-12-11 18:35:45 +0100

    first day

--- src/advent/2018.clj
+++ src/advent/2018.clj
@@ -1,5 +1,8 @@
 (ns advent.2018
-  (:require [advent.core :as a]))
+  (:require [advent.core :as a]
+            [clojure.java.io :as io]))
 
 (a/defcase day1 "2018/1.input.txt"
-  (println (read *in*)))
+  (reduce +
+      (map #(Integer/parseInt %) 
+        (line-seq (io/reader *in*)))))
