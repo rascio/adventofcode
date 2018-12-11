34080def0904eaafaafee37031b7feb6946edccc
Manuel Rascioni <mrascioni@hotels.com>
2018-12-11 18:40:22 +0100

    day-part2

--- src/advent/2018.clj
+++ src/advent/2018.clj
@@ -2,7 +2,11 @@
   (:require [advent.core :as a]
             [clojure.java.io :as io]))
 
+;https://adventofcode.com/2018/day/1
 (a/defcase day1 "2018/1.input.txt"
   (reduce +
-      (map #(Integer/parseInt %) 
+      (map #(Integer/parseInt %)
         (line-seq (io/reader *in*)))))
+
+(a/defcase day1-part2 "2018/1.input.txt"
+  (TODO))
--- src/advent/core.clj
+++ src/advent/core.clj
@@ -1,7 +1,13 @@
 (ns advent.core
   (:require [clojure.java.io :as io]))
 
-(defmacro defcase [name file body]
+(defmacro defcase
+  "Define a function binding the *in*
+  to the content of the `file` read from classpath, eg:
+      (defcase test \"position/in/classpath.txt\"
+          (println (read-line)) ;Prints the first line of the file
+  "
+  [name file body]
   `(defn ~name []
      (binding [*in* (->> ~file
                       io/resource
