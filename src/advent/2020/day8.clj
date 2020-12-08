c11c07b093ac229d51260090662cc2c584ca432c
Manuel Rascioni <mrascioni@hotels.com>
2020-12-09 00:54:12 +0100

    day8

--- src/advent/2020/day8.clj
+++ src/advent/2020/day8.clj
@@ -13,28 +13,22 @@
 (defmethod exec :nop [[_ _] idx acc] [(inc idx) acc])
 (defmethod exec :acc [[_ n] idx acc] [(inc idx) (+ n acc)])
 (defmethod exec :jmp [[_ n] idx acc] [(+ n idx) acc])
-(defmethod exec nil [& _] nil)
-(defmethod exec :default [[code] & _] (throw (ex-info "Not Implemented" {:code code})))
 
 (defn find-loop 
-  ([instructions] (find-loop instructions
-                             [0 0] 
-                             #{}))
+  ([instructions] (find-loop instructions [0 0] #{}))
   ([instructions state s]
    (loop [[idx acc] state
           seen s]
      (let [op (get instructions idx)]
-       (if (nil? op)
-         [:res idx acc]
-         (let [[next-idx :as step] (exec op idx acc)]
-           (if (contains? seen next-idx)
-             [:loop idx acc]
-             (->> (conj seen idx)
-                  (recur step)))))))))
+       (cond (nil? op) [:res idx acc]
+             (contains? seen idx) [:loop idx acc]
+             :else (->> (conj seen idx)
+                        (recur (exec op idx acc))))))))
 
 (defn part1 []
   (->> input
        (map parse-op)
+       (vec)
        (find-loop)
        (last)))
 
