560d4fec9470e4205b5aec680544aa7950861301
Manuel Rascioni <mrascioni@hotels.com>
2021-12-19 22:59:17 +0100

    16 2021

--- src/advent/2021/day16.clj
+++ src/advent/2021/day16.clj
@@ -6,8 +6,9 @@
 (def input (->> (first (reader))))
 
 (def sub-packets-n (atom 0))
-(defn log-padding [] (apply str (repeat @sub-packets-n "\t")))
-(def log (a/debugger false))
+(def log (a/debugger true))
+(defn log-padded [& args] 
+  (apply log (apply str (repeat @sub-packets-n "\t")) args))
 
 (declare read-packets)
 
@@ -17,18 +18,17 @@
     (format "%4s" $)
     (.replace $ " " "0")))
 
-(defn hex->code [hex] [0 (mapcat hex->binary hex)])
+(defn hex->code [hex] (mapcat hex->binary hex))
 (defn binary->int [v] (Long/parseLong (apply str v) 2))
 
 
-(defn str-io [[r b]] [r (apply str b)])
-(defn end? [[_ bits]] (empty? bits))
-(defn consume [n [c b]] [(+ c n) (drop n b)])
+(defn str-io [bits] (apply str bits))
+(defn end? [bits] (empty? bits))
 (defn read-io 
   ([n io] (read-io n identity io))
-  ([n f [c bytes]]
-   (let [[head tail] (split-at n bytes)]
-     [(f head) [(+ c n) tail]])))
+  ([n f bits]
+   (let [[head tail] (split-at n bits)]
+     [(f head) tail])))
 (defn read-io-int [n io]
   (read-io n binary->int io))
 
@@ -36,12 +36,11 @@
   [io]
   (loop [[[control & part] io] (read-io 5 io)
          acc ""]
-    (log (log-padding) "control:" control (apply str part))
-    (when (nil? control) (throw (ex-info "BOOM" {:code (str-io io)})))
+    (log-padded "control:" control (apply str part))
     (as-> (apply str acc part) $
       (if (= \0 control)
         (do
-          (log (log-padding) "literal:" (Long/parseLong $ 2))
+          (log-padded "literal:" (Long/parseLong $ 2))
           [(Long/parseLong $ 2) io])
         (recur (read-io 5 io) $)))))
 
@@ -52,23 +51,13 @@
   (let [[[length-type-id] IO] (read-io 1 IO)]
     (case length-type-id
       \0 (let [[packets-size IO] (read-io-int 15 IO)
-               _ (log (log-padding) "packets-size:" packets-size)
-               [packets-binary IO] (read-io packets-size IO)
-               [packets _] (read-packets [0 packets-binary])]
+               [[packets] IO] (read-io packets-size read-packets IO)]
+           (log-padded "packets-size:" packets-size "packets:" packets)
            [packets IO])
       \1 (let [[n-packets IO] (read-io-int 11 IO)]
-           (log (log-padding) "packets:" n-packets)
+           (log-padded "packets:" n-packets)
            (read-packets IO n-packets))
       (throw (ex-info (str "Not valid:" length-type-id) {})))))
-
-(defn exec-operation
-  [IO operation]
-  (let [[subpackets IO] (read-io-sub-packets IO)
-        _ (log (log-padding) "subpackets:" subpackets (str-io IO))
-        res (->> subpackets
-                 (map (fn [[_ _ v]] v))
-                 (apply operation))]
-    [res IO]))
 
 (defn greater-than [& values] (if (apply < values) 1 0))
 (defn less-than [& values] (if (apply > values) 1 0))
@@ -80,8 +69,7 @@
          packets []
          IO IO
          processed 0]
-    ;(a/log-forms log state max-packets processed (count code))
-    (a/trap IO (log (log-padding) "state:" state "packets" (str processed "/" max-packets) (str-io IO)))
+    (log-padded "state:" state "packets" (str processed "/" max-packets) packets "bits:" (str-io IO))
     (case state
       :next (if (or (end? IO)
                     (and (some? max-packets)
@@ -90,65 +78,28 @@
               (recur :header packets IO processed))
       :header (let [[version IO] (read-io-int 3 IO)
                     [type-id IO] (read-io-int 3 IO)]
-                (log (log-padding) "version:" version "type-id:" type-id)
-                (recur [:type type-id]
+                (log-padded "version:" version "type-id:" type-id)
+                (recur (if (= 4 type-id) :literal :operation)
                        (cons [version type-id] packets)
                        IO
                        processed))
-      [:type 4] (let [[value IO] (read-io-literal IO)
-                      [current & others] packets]
-                  (recur :next
-                         (cons (conj current value) others)
-                         IO
-                         (inc processed)))
-      [:type 0] (let [[res IO] (exec-operation IO +)
-                      [current & others] packets]
-                  (recur :next
-                         (cons (conj current res) others)
-                         IO
-                         (inc processed)))
-      [:type 1] (let [[res IO] (exec-operation IO *)
-                      [current & others] packets]
-                  (recur :next
-                         (cons (conj current res) others)
-                         IO
-                         (inc processed)))
-      [:type 2] (let [[res IO] (exec-operation IO min)
-                      [current & others] packets]
-                  (recur :next
-                         (cons (conj current res) others)
-                         IO
-                         (inc processed)))
-      [:type 3] (let [[res IO] (exec-operation IO max)
-                      [current & others] packets]
-                  (recur :next
-                         (cons (conj current res) others)
-                         IO
-                         (inc processed)))
-      [:type 5] (let [[res IO] (exec-operation IO greater-than)
-                      [current & others] packets]
-                  (recur :next
-                         (cons (conj current res) others)
-                         IO
-                         (inc processed)))
-      [:type 6] (let [[res IO] (exec-operation IO less-than)
-                      [current & others] packets]
-                  (recur :next
-                         (cons (conj current res) others)
-                         IO
-                         (inc processed)))
-      [:type 7] (let [[res IO] (exec-operation IO equals-to)
-                      [current & others] packets]
-                  (recur :next
-                         (cons (conj current res) others)
-                         IO
-                         (inc processed)))
-      [packets IO])))
-(defn hex->packets 
+      :literal (let [[value IO] (read-io-literal IO)
+                     [current & others] packets]
+                 (recur :next
+                        (cons (conj current value) others)
+                        IO
+                        (inc processed)))
+      :operation (let [[subpackets IO] (read-io-sub-packets IO)
+                       [current & others] packets]
+                   (log-padded "subpackets:" subpackets (str-io IO))
+                   (recur :next
+                          (cons (conj current subpackets) others)
+                          IO
+                          (inc processed))))))
+(defn hex->packet
   [hex]
   (-> (hex->code hex)
       (read-packets 1)
-      (first)
       (first)))
 (defn add-version
   [packets]
@@ -156,7 +107,7 @@
    (fn [acc [version _ packets]]
      (+ acc
         version
-        (if (seq? packets)
+        (if (seqable? packets)
           (add-version packets)
           0)))
    0
@@ -165,7 +116,7 @@
 ; This will need to change the read-packets to accumulate them instead of doing the calculation
 (defn part1 [& [code]]
   (let [c (or code input)
-        packets (hex->packets c)]
+        packets (hex->packet c)]
     (add-version packets)))
 
 (defn test-part1 []
@@ -174,9 +125,23 @@
   (assert (= 23 (part1 "C0015000016115A2E0802F182340")))
   (assert (= 31 (part1 "A0016C880162017C3686B18A3D4780"))))
 
+(def operations {0 +
+                 1 *
+                 2 min
+                 3 max
+                 5 greater-than
+                 6 less-than
+                 7 equals-to})
+(defn exec [[_ type-id value]]
+  (if (= 4 type-id)
+    value
+    (->> value
+         (map exec)
+         (apply (operations type-id)))))
+
 (defn part2 [& [code]]
   (let [c (or code input)]
-    (last (hex->packets c))))
+    (exec (first (hex->packet c)))))
   
 (defn test-part2 []
   (assert (= 3 (part2 "C200B40A82")))
